a2b12e4b0d36e085f0fc432b5da487ee96dbb845
brian.mckenna <brian.mckenna@datto.com>
2023-08-22 10:54:23 +0100

    add labels to skill form

--- src/components/skills/SkillForm.tsx
+++ src/components/skills/SkillForm.tsx
@@ -75,20 +75,23 @@ export const SkillForm: React.FC<SkillFormProps> = ({
   return (
     <Form
       form={form}
+      layout={"vertical"}
       name={initialSkill ? "edit_skill" : "add_skill"}
       className="skill-form"
       initialValues={initialSkill || { remember: true }}
       onFinish={onFinish}
     >
       <Form.Item
+        label="Skill name"
         name="name"
-        rules={[{ required: true, message: "Please input name" }]}
+        rules={[{ required: true, message: "Please input skill name" }]}
       >
         <Input placeholder="name" />
       </Form.Item>
       <Form.Item
+        label="Description"
         name="description"
-        rules={[{ required: true, message: "Please input description" }]}
+        rules={[{ required: true, message: "Please input skill description" }]}
       >
         <TextArea rows={3} placeholder="description" />
       </Form.Item>
